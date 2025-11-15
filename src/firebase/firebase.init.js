ba590540d6961d432a14c9418359a74b389d4a23
Kabir Hossain <nerob2308@gmail.com>
2025-11-15 11:39:15 +0600

    secrate file push

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,20 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFgNa20lGcTZIkHh3buBHHY_25cnAd0ds",
-  authDomain: "artify-project-c5361.firebaseapp.com",
-  projectId: "artify-project-c5361",
-  storageBucket: "artify-project-c5361.firebasestorage.app",
-  messagingSenderId: "431478180421",
-  appId: "1:431478180421:web:f63ede1721b3e9a76ef12c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
